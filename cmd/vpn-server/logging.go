e355efa88057abd1604fa454f7ae0cfa1c73b40f
Vladimir Gromov <108759867+alexmorettihhhh@users.noreply.github.com>
2025-02-12 03:05:59 +0300

    Update logging.go
    Глобальный логгер : Мы создали глобальную переменную logger, которую инициализируем в функции init(). Это позволяет централизованно управлять настройками логгера.
    Формат JSON : Мы установили формат вывода логов как JSON с помощью logrus.JSONFormatter. Это делает логи более удобными для анализа в системах мониторинга (например, ELK, Splunk).
    Структурированное логирование : Мы добавили возможность передавать дополнительные поля в логи через параметр fields. Это позволяет добавлять контекст к каждому сообщению (например, идентификатор пользователя, имя функции и т.д.).
    Контекст : Мы добавили поддержку контекста (context.Context) в функции логирования. Это полезно, если вы хотите передавать информацию о запросе или другие данные через контекст.
    Уровень логирования : Мы установили уровень логирования InfoLevel, но вы можете легко изменить его на DebugLevel, WarnLevel и т.д., в зависимости от ваших потребностей

--- cmd/vpn-server/logging.go
+++ cmd/vpn-server/logging.go
@@ -1,15 +1,51 @@
 package main
 
 import (
-	"github.com/sirupsen/logrus"
+    "context"
+    "github.com/sirupsen/logrus"
 )
 
-func logError(err error) {
-	if err != nil {
-		logrus.Error(err)
-	}
+// Глобальный логгер
+var logger *logrus.Logger
+
+func init() {
+    // Инициализация логгера
+    logger = logrus.New()
+    logger.SetFormatter(&logrus.JSONFormatter{}) // Устанавливаем формат JSON для логов
+    logger.SetLevel(logrus.InfoLevel)           // Устанавливаем уровень логирования
 }
 
-func logInfo(message string) {
-	logrus.Info(message)
+// logError логирует ошибку с дополнительными полями
+func logError(ctx context.Context, err error, fields map[string]interface{}) {
+    if err != nil {
+        fields["error"] = err.Error()
+        logger.WithContext(ctx).WithFields(fields).Error("An error occurred")
+    }
+}
+
+// logInfo логирует информационное сообщение с дополнительными полями
+func logInfo(ctx context.Context, message string, fields map[string]interface{}) {
+    logger.WithContext(ctx).WithFields(fields).Info(message)
+}
+
+func main() {
+    // Пример использования
+    ctx := context.Background()
+
+    // Логирование информационного сообщения
+    logInfo(ctx, "Application started", map[string]interface{}{
+        "version": "1.0.0",
+        "env":     "production",
+    })
+
+    // Логирование ошибки
+    err := someFunctionThatMightFail()
+    logError(ctx, err, map[string]interface{}{
+        "function": "someFunctionThatMightFail",
+    })
+}
+
+func someFunctionThatMightFail() error {
+    // Пример функции, которая может вернуть ошибку
+    return nil // или return errors.New("something went wrong")
 }
